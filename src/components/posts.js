2809f4cc9d54e3ca5e52c9b6ca49dc731be3551a
Sami <mohammadsamimsu@gmail.com>
2021-09-10 23:16:11 +0400

    Change visited link colors

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,24 +1,32 @@
 import * as React from "react";
 import { Link } from "gatsby";
-import { postLink } from "../pages/index.module.css";
+import { postLink, postLinkContainer } from "../pages/index.module.css";
 
 const Posts = () => {
   return (
     <div>
-      <div className={postLink}>
-        <Link to="/posts/advice">Advice</Link>
+      <div className={postLinkContainer}>
+        <Link to="/posts/advice" className={postLink}>
+          Advice
+        </Link>
         <span>Sep 10, 2021</span>
       </div>
-      <div className={postLink}>
-        <Link to="/posts/content-creator">Content Creator</Link>
+      <div className={postLinkContainer}>
+        <Link to="/posts/content-creator" className={postLink}>
+          Content Creator
+        </Link>
         <span>Sep 9, 2021</span>
       </div>
-      <div className={postLink}>
-        <Link to="/posts/introvert">Introvert</Link>
+      <div className={postLinkContainer}>
+        <Link to="/posts/introvert" className={postLink}>
+          Introvert
+        </Link>
         <span>Sep 8, 2021</span>
       </div>
-      <div className={postLink}>
-        <Link to="/posts/hi-im-sami">Hi! I'm Sami</Link>
+      <div className={postLinkContainer}>
+        <Link to="/posts/hi-im-sami" className={postLink}>
+          Hi! I'm Sami
+        </Link>
         <span>Sep 7, 2021</span>
       </div>
     </div>
