98562bca6780f670b6e28e7a3a3f7faa6886771e
Sami <mohammadsamimsu@gmail.com>
2021-09-11 22:57:04 +0400

    Create new post

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,32 +1,28 @@
 import * as React from "react";
 import { Link } from "gatsby";
-import { postLink, postLinkContainer } from "../pages/index.module.css";
+import { postLinkContainer } from "../pages/index.module.css";
 
 const Posts = () => {
   return (
     <div>
       <div className={postLinkContainer}>
-        <Link to="/posts/advice" className={postLink}>
-          Advice
-        </Link>
+        <Link to="/posts/birthday">Birthday</Link>
+        <span>Sep 11, 2021</span>
+      </div>
+      <div className={postLinkContainer}>
+        <Link to="/posts/advice">Advice</Link>
         <span>Sep 10, 2021</span>
       </div>
       <div className={postLinkContainer}>
-        <Link to="/posts/content-creator" className={postLink}>
-          Content Creator
-        </Link>
+        <Link to="/posts/content-creator">Content Creator</Link>
         <span>Sep 9, 2021</span>
       </div>
       <div className={postLinkContainer}>
-        <Link to="/posts/introvert" className={postLink}>
-          Introvert
-        </Link>
+        <Link to="/posts/introvert">Introvert</Link>
         <span>Sep 8, 2021</span>
       </div>
       <div className={postLinkContainer}>
-        <Link to="/posts/hi-im-sami" className={postLink}>
-          Hi! I'm Sami
-        </Link>
+        <Link to="/posts/hi-im-sami">Hi! I'm Sami</Link>
         <span>Sep 7, 2021</span>
       </div>
     </div>
