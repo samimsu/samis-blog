8137c515e9c83ad59ef218897b12b1f3a7b64481
Sami <mohammadsamimsu@gmail.com>
2021-09-11 23:19:11 +0400

    Create count prop

--- src/components/posts.js
+++ src/components/posts.js
@@ -2,31 +2,28 @@ import * as React from "react";
 import { Link } from "gatsby";
 import { postLinkContainer } from "../pages/index.module.css";
 
-const Posts = () => {
+const Post = ({ file, linkText, date }) => {
   return (
-    <div>
-      <div className={postLinkContainer}>
-        <Link to="/posts/birthday">Birthday</Link>
-        <span>Sep 11, 2021</span>
-      </div>
-      <div className={postLinkContainer}>
-        <Link to="/posts/advice">Advice</Link>
-        <span>Sep 10, 2021</span>
-      </div>
-      <div className={postLinkContainer}>
-        <Link to="/posts/content-creator">Content Creator</Link>
-        <span>Sep 9, 2021</span>
-      </div>
-      <div className={postLinkContainer}>
-        <Link to="/posts/introvert">Introvert</Link>
-        <span>Sep 8, 2021</span>
-      </div>
-      <div className={postLinkContainer}>
-        <Link to="/posts/hi-im-sami">Hi! I'm Sami</Link>
-        <span>Sep 7, 2021</span>
-      </div>
+    <div className={postLinkContainer}>
+      <Link to={`/posts/${file}`}>{linkText}</Link>
+      <span>{date}</span>
     </div>
   );
+};
+
+const Posts = ({ count }) => {
+  const posts = [
+    <Post file="birthday" linkText="Birthday" date="Sep 11, 2021" />,
+    <Post file="advice" linkText="Advice" date="Sep 10, 2021" />,
+    <Post
+      file="content-creator"
+      linkText="Content Creator"
+      date="Sep 9, 2021"
+    />,
+    <Post file="introvert" linkText="Introvert" date="Sep 8, 2021" />,
+    <Post file="hi-im-sami" linkText="Hi! I'm Sami" date="Sep 7, 2021" />,
+  ];
+  return <div>{count ? posts.slice(0, count) : posts}</div>;
 };
 
 export default Posts;
