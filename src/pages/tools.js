31c7aeb68b6545c5909f9ba2b06fa560f54fef52
Sami <mohammadsamimsu@gmail.com>
2021-09-11 22:57:20 +0400

    Update tools.js

--- src/pages/tools.js
+++ src/pages/tools.js
@@ -1,13 +1,13 @@
 import * as React from "react";
 import Layout from "../components/layout";
 import { Link } from "gatsby";
-import { postLink } from "./index.module.css";
+import { postLinkContainer } from "./index.module.css";
 
 const tools = () => {
   return (
     <Layout pageTitle="Tools">
       <p>Tools and scripts that I've created</p>
-      <div className={postLink}>
+      <div className={postLinkContainer}>
         <Link to="/tools/paragraphs-to-html">Paragraphs to HTML</Link>
       </div>
     </Layout>
