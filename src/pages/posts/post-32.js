a6cd7d66ec11d5d4f18749766956aa560de6b74a
Sami <mohammadsamimsu@gmail.com>
2021-10-08 13:04:05 +0400

    Create new post

---
+++ src/pages/posts/post-32.js
@@ -0,0 +1,37 @@
+import * as React from "react";
+import Layout from "../../components/layout";
+
+const Post32 = () => {
+  return (
+    <Layout pageTitle="#21">
+      <h4>October 8, 2021</h4>
+      <article>
+        <p>
+          This post will be up earlier than usual since I will probably be out
+          until pretty late. We're going to visit a cousin, and I don't know if
+          I will be back before midnight or have the energy to write a post. I
+          will probably miss out on walking too.
+        </p>
+        <p>
+          I ran a few errands in the morning. It was nice to drive again. I
+          don't get to do that often. It was pretty dangerous without a side
+          mirror. We'll get that fixed soon.
+        </p>
+        <p>
+          I read a bit more of No Rules Rules. It's a compelling read. I think
+          it goes against traditional wisdom about how to run a company
+          effectively. Netflix has been incredibly successful, so they've earned
+          the right to talk about radical company policies. The employees also
+          seem to like working there.
+        </p>
+        <p>
+          So far, the principles have sounded appealing to me. It's about
+          concentrating talent and reducing control over them. It's about having
+          as few policies as possible. I like the sound of that.
+        </p>
+      </article>
+    </Layout>
+  );
+};
+
+export default Post32;
