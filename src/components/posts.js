08cde119c3e9fd5e91f8ab337a473ab2287b29cc
Sami <mohammadsamimsu@gmail.com>
2021-10-20 23:00:57 +0400

    Create new post

--- src/components/posts.js
+++ src/components/posts.js
@@ -13,6 +13,7 @@ const Post = ({ file, linkText, date }) => {
 
 const Posts = ({ count }) => {
   const posts = [
+    <Post file="post-44" linkText="#44" date="Oct 20, 2021" />,
     <Post file="post-43" linkText="#43 - 20,000 steps" date="Oct 19, 2021" />,
     <Post file="post-42" linkText="#42" date="Oct 18, 2021" />,
     <Post file="post-41" linkText="#41" date="Oct 17, 2021" />,
