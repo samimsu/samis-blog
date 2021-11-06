4049ae97ba7877b5db86a1a49b0613600a579ce4
Sami <mohammadsamimsu@gmail.com>
2021-11-06 22:16:47 +0400

    Create new post

---
+++ src/pages/posts/post-61.js
@@ -0,0 +1,42 @@
+import * as React from "react";
+import Layout from "../../components/layout";
+
+const Post61 = () => {
+  return (
+    <Layout pageTitle="#61">
+      <h4>November 6, 2021</h4>
+      <article>
+        <p>
+          It's harder to keep up these posts with a job. Who knew a job would
+          take up most of your time? It's alright. I hope this job goes well.
+          I've been trying.
+        </p>
+        <p>
+          I don't think I'll be able to make 10k steps today. It's alright,
+          though. It's taking me longer to get this post done because the TV is
+          on in the background.
+        </p>
+        <p>
+          I haven't been reading either. I don't know if I'll reach all my goals
+          this month. At least I got the main one done. I hope I can keep hold
+          of this job and not get too overwhelmed by it.
+        </p>
+        <p>
+          I had a decent day today too. I managed to get a sale in by the end of
+          the day. I make dozens and dozens of calls, and I'm lucky if I make a
+          sale.
+        </p>
+        <p>
+          It's a fascinating experience, though. An interesting chapter for my
+          life. I'm glad I'm getting some sales experience. I know salespeople
+          can be pretty annoying, so I understand when potential clients get
+          annoyed with us. I try to be as nice as possible. Some people just
+          hang up in the middle of the conversation. It's a cool learning
+          experience.
+        </p>
+      </article>
+    </Layout>
+  );
+};
+
+export default Post61;
